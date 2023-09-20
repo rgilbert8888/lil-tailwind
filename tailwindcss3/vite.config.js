f045e46ccc6ee0c8ca55a7126c2b9035ba2c5e98
Robyn Gilbert <rgilbert@adhq.com>
2023-09-20 09:41:42 -0400

    Initial Commit

---
+++ tailwindcss3/vite.config.js
@@ -0,0 +1,17 @@
+export default {
+    root: 'src',
+    publicDir: 'public',
+    outDir:'../docs',
+    server: {
+        open:true
+    },
+    // build: {
+    //     rollupOptions: {
+    //         output: {
+    //             entryFileNames: `assets/[name].js`,
+    //             chunkFileNames: `assets/[name].js`,
+    //             assetFileNames: `assets/[name].[ext]`
+    //         }
+    //     }
+    // }
+}
